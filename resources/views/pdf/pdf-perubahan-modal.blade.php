e8346145c5d5e766ebc658ea520edeba356d4b4b
Ionichi <feryandi2303@gmail.com>
2024-06-11 08:57:40 +0700

    fix(hotfix): error export laporan on vercel

--- resources/views/pdf/pdf-perubahan-modal.blade.php
+++ resources/views/pdf/pdf-perubahan-modal.blade.php
@@ -4,24 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
 
     <title>Laporan Perubahan Modal - {{ $dataUsaha['nama'] }}</title>
 </head>
 <style>
-@font-face {
-    font-family: 'Poppins';
-    font-style: normal;
-    font-weight: 400;
-    font-display: swap;
-    src: url(https://fonts.gstatic.com/s/poppins/v21/pxiEyp8kv8JHgFVrJJfecg.woff2) format('woff2');
-    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-}
 body {
-    font-family: Poppins, sans-serif;
+    font-family: Arial, sans-serif;
 }
 .header {
     text-align: center;
